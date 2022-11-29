a1e3da1530fbb7b6a60f06e2a40e6dca2666ba27
AlenaNovik <elena.pigaleva@gmail.com>
2022-11-29 16:25:59 +0300

    fs create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import { open, close, writeFile } from "fs";
+
+const PATH = "src/fs/files";
+const FILENAME = "fresh.txt";
+
 const create = async () => {
-    // Write your code here 
+  open(`${PATH}/${FILENAME}`, "wx", (err, fd) => {
+    if (err) {
+      if (err.code === "EEXIST") {
+        console.error("FS operation failed");
+        return;
+      }
+      throw err;
+    }
+
+    try {
+      writeFile(`${PATH}/${FILENAME}`, "I am fresh and young", (err) => {
+        if (err) throw err;
+        console.log("File fresh was created");
+      });
+    } finally {
+      close(fd, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
 };
 
 await create();
