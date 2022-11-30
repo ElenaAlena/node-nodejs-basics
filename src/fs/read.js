0abe0a83cd03863a44a1206ff5f57e0e5e6fd086
ElenaNovik <alena.novik@itechart-group.com>
2022-11-30 16:08:54 +0300

    basics fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+import { open, close, readFile } from "fs";
+
+const PATH = "src/fs/files";
+const FILENAME = "fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+  open(`${PATH}/${FILENAME}`, "r", (err, fd) => {
+    if (err && err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+
+    try {
+      readFile(`${PATH}/${FILENAME}`, "utf8", (err, data) => {
+        if (err) throw err;
+        console.log(data);
+      });
+    } finally {
+      close(fd, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
 };
 
 await read();
