95f168452650746856561ed2b545689234bbbc74
ElenaNovik <alena.novik@itechart-group.com>
2022-11-30 21:36:46 +0300

    Command line interface(src/cli)

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const env = process.env;
+    const filtered = Object.keys(env).filter(key => key.startsWith("RSS_"));
+    filtered.forEach(item=>{
+        console.log(`${item}=${process.env[item]}`)
+    })
+    ;
 };
 
 parseEnv();
