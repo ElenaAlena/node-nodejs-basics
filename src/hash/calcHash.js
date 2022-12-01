f5e3830a9e280bb18e20de9ccbad7182c46f9f71
ElenaNovik <alena.novik@itechart-group.com>
2022-12-01 16:29:29 +0300

    Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,41 @@
+import { open, close, readFile } from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { createHash } from "crypto";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+const PATH = `${__dirname}/files`;
+const FILENAME = "fileToCalculateHashFor.txt";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+
+  hash.on("readable", () => {
+    const data = hash.read();
+    if (data) {
+      console.log(data.toString("hex"));
+    }
+  });
+  open(`${PATH}/${FILENAME}`, "r", (err, fd) => {
+    if (err && err.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+
+    try {
+      readFile(`${PATH}/${FILENAME}`, "utf8", (err, data) => {
+        if (err) throw err;
+        hash.write(data);
+        hash.end();
+      });
+    } finally {
+      close(fd, (err) => {
+        if (err) throw err;
+      });
+    }
+  });
 };
 
 await calculateHash();
