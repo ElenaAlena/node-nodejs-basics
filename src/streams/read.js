dd7e48fbcc4620f860275f65c7bf26ef79f4f61b
ElenaNovik <alena.novik@itechart-group.com>
2022-12-01 23:21:37 +0300

    Streams (src/streams)

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
-const read = async () => {
-    // Write your code here 
-};
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+const PATH = `${__dirname}/files/fileToRead.txt`;
+
+const read = async () => {
+  const readStream = fs.createReadStream(PATH);
+  readStream.pipe(process.stdout);
+};
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+const PATH = `${__dirname}/files/fileToWrite.txt`;
+
 const write = async () => {
-    // Write your code here 
+
+    const writeStream = fs.createWriteStream(PATH);    
+    process.stdin.pipe(writeStream);
+    process.stdin.resume();
 };
 
 await write();
