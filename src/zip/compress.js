784a5e931840e4ba7dfbc9f6495138a4ab4f5f2e
ElenaAlena <elena.pigaleva@gmail.com>
2022-12-02 11:32:09 +0300

    Zlib (src/zip)

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import zlib from "zlib";
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+const DEARCHIVE_PATH = `${__dirname}/files/fileToCompress.txt`;
+const ARCHIVE_PATH = `${__dirname}/files/archive.gz`;
+
 const compress = async () => {
-    // Write your code here 
+  var gzip = zlib.createGzip();
+  var readStream = fs.createReadStream(DEARCHIVE_PATH);
+  var writeStream= fs.createWriteStream(ARCHIVE_PATH);
+  readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import zlib from "zlib";
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+const DEARCHIVE_PATH = `${__dirname}/files/fileToCompress.txt`;
+const ARCHIVE_PATH = `${__dirname}/files/archive.gz`;
+
 const decompress = async () => {
-    // Write your code here 
+  const inputFile = fs.createReadStream(ARCHIVE_PATH);
+  const outputFile = fs.createWriteStream(DEARCHIVE_PATH);
+
+  inputFile.pipe(zlib.createUnzip()).pipe(outputFile);
 };
 
 await decompress();
