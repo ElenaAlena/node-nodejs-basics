964375253e3c58c41c3f1f56a519759727e4f3b8
ElenaAlena <elena.pigaleva@gmail.com>
2022-12-04 21:42:02 +0300

    Child Processes (src/cp)

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import { fork } from "child_process";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+const PATH = `${__dirname}/files/script.js`;
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = fork(PATH, args);
 };
 
-spawnChildProcess();
+spawnChildProcess(["hello", "hello2"]);
