5eed122fbeef5c3e6182dd61acda0a4e26fba1a5
ElenaAlena <elena.pigaleva@gmail.com>
2022-12-05 21:52:16 +0300

    copy changed to promise

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,4 +1,4 @@
-import { cp, stat } from "fs";
+import { cp, stat } from "fs/promises";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -6,17 +6,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const PATH = `${__dirname}/files`;
-const NEWPATH = `${__dirname}files_copy`;
+const NEWPATH = `${__dirname}/files_copy`;
 
 const copy = async () => {
-  stat(NEWPATH, (err) => {
-    if (err && err.code === "ENOENT") {
-      cp(PATH, NEWPATH, { recursive: true }, (err) => {
-        if (err) throw err;
-        console.log("Success");
-      });
+  try {
+    await stat(NEWPATH);
+    throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      cp(PATH, NEWPATH, { recursive: true })
+        .then(() => {
+          console.log("Success");
+        })
+        .catch((err) => {
+          throw err;
+        });
     } else throw new Error("FS operation failed");
-  });
+  }
 };
 
 copy();
